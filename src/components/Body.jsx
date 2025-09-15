93b6018635e0af7b40d482b5249aa5eb3610514e
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-15 23:08:57 +0530

    ResturantCards component created

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResturantCards from "./ResturantCards";
+
+const Body = () => {
+  return (
+    <>
+      <ResturantCards />
+    </>
+  );
+};
+
+export default Body;
---
+++ src/components/ResturantCards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResturantCards = () => {
+  return (
+    <>
+      <h6>Meghna Foods</h6>
+    </>
+  );
+};
+
+export default ResturantCards;
