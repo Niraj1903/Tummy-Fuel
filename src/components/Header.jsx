0e33cc86047db84025cef08d84fc474f3dfcaa7e
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-17 20:08:54 +0530

    Logo issue fix on netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [btn, setBtn] = useState("Login");
   return (
     <>
       <div className="flex items-center justify-between p-4 bg-white shadow-md">
-        {/* Logo */}
-        <img className="h-10 w-auto" src="/public/tummyFuel.png" alt="logo" />
+        <img className="h-10 w-auto" src="/tummyFuel.png" alt="logo" />
 
-        {/* Navigation Links */}
         <ul className="flex space-x-6 text-gray-800">
           <li className="cursor-pointer hover:text-orange-500">Home</li>
           <li className="cursor-pointer hover:text-orange-500">About Us</li>
           <li className="cursor-pointer hover:text-orange-500">Contact Us</li>
           <li className="cursor-pointer hover:text-orange-500">Cart</li>
-          <li className="cursor-pointer hover:text-orange-500">Login</li>
+          <li className="cursor-pointer hover:text-orange-500">
+            <button onClick={() => setBtn(btn == "Login" ? "Logout" : "Login")}>
+              {btn}
+            </button>
+          </li>
         </ul>
       </div>
     </>
